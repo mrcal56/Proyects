12a6f3aa3a848c2dbd1571a7963cee89412da0e8
mrcal56 <marlon56r@gmail.com>
2024-06-04 22:54:06 -0600

    -

---
+++ BuscadorDeCanciones.py
@@ -0,0 +1,571 @@
+#crear diccionario
+travis = {
+    'Sicko Mode' : """
+Astro, yeah
+Sun is down, freezin' cold
+That's how we already know winter's here
+My dawg would prolly do it for a Louis belt
+That's just all he know, he don't know nothin' else
+I tried to show 'em, yeah
+I tried to show 'em, yeah, yeah
+Yeah, yeah, yeah
+Gone on you with the pick and roll
+Young LaFlame, he in sicko mode
+Woo, made this here with all the ice on in the booth
+At the gate outside, when they pull up, they get me loose
+Yeah, Jump Out boys, that's Nike boys, hoppin' out coupes
+This shit way too big, when we pull up give me the loot
+(Gimme the loot)
+Was off the Remy, had a Papoose
+Had to hit my old town to duck the news
+Two-four hour lockdown, we made no moves
+Now it's 4AM and I'm back up poppin' with the crew
+I just landed in, Chase B mixes pop like Jamba Juice
+Different colored chains, think my jeweler really sellin' fruits
+And they chokin', man, know the crackers wish it was a noose
+Some-some-some, someone said
+To win the retreat, we all in too deep
+P-p-playin' for keeps, don't play us for weak (someone said)
+To win the retreat, we all in too deep
+P-p-playin' for keeps, don't play us for weak (yeah)
+This shit way too formal, y'all know I don't follow suit
+Stacey Dash, most of these girls ain't got a clue
+All of these hoes I made off records I produced
+I might take all my exes and put 'em all in a group
+Hit my esés, I need the bootch
+'Bout to turn this function to Bonnaroo
+Told her, "Hop in, you comin' too"
+In the 305, bitches treat me like I'm Uncle Luke
+(Don't stop, pop that pussy)
+Had to slop the top off, it's just a roof (uh)
+She said, "Where we goin'?" I said, "The moon"
+We ain't even make it to the room
+She thought it was the ocean, it's just the pool
+Now I got her open, it's just the Goose
+Who put this shit together? I'm the glue (someone said)
+Shorty FaceTimed me out the blue
+Someone said (playin'-playin' for keeps)
+Someone said, motherfuck what someone said
+(Don't play us for weak)
+Yeah
+Astro
+Yeah, yeah
+Tay Keith, fuck these niggas up (ayy, ayy)
+She's in love with who I am
+Back in high school, I used to bus it to the dance (yeah)
+Now I hit the FBO with duffles in my hands
+I did half a Xan, thirteen hours 'til I land
+Had me out like a light, ayy, yeah
+Like a light, ayy, yeah
+Like a light, ayy
+Slept through the flight, ayy
+Knocked for the night, ayy, 767, man
+This shit got double bedroom, man
+I still got scores to settle, man
+I crept down the block (down the block), made a right (yeah, right)
+Cut the lights (yeah, what?), paid the price (yeah)
+Niggas think it's sweet (nah, nah), it's on sight (yeah, what?)
+Nothin' nice (yeah), baguettes in my ice (aww, man)
+Jesus Christ (yeah), checks over stripes (yeah)
+That's what I like (yeah), that's what we like (yeah)
+Lost my respect, you not a threat
+When I shoot my shot, that shit wetty like I'm Sheck (bitch)
+See the shots that I took (ayy), wet like I'm Book (ayy)
+Wet like I'm Lizzie, I be spinnin' Valley
+Circle blocks 'til I'm dizzy (yeah, what?)
+Like where is he? (Yeah, what?)
+No one seen him (yeah, yeah)
+I'm tryna clean 'em (yeah)
+She's in love with who I am
+Back in high school, I used to bus it to the dance
+Now I hit the FBO with duffles in my hand (woo)
+I did half a Xan, thirteen hours 'til I land
+Had me out like a light, like a light
+Like a light, like a light
+Like a light (yeah), like a light
+Like a light
+Yeah, passed the dawgs a celly
+Sendin' texts, ain't sendin' kites, yeah
+He said, "Keep that on lock"
+I said, "You know this shit, it's stife", yeah
+It's absolute (yeah), I'm back reboot (it's lit)
+LaFerrari to Jamba Juice, yeah (skrrt, skrrt)
+We back on the road, they jumpin' off, no parachute, yeah
+Shawty in the back
+She said she workin' on her glutes, yeah (oh my God)
+Ain't by the book, yeah
+This how it look, yeah
+'Bout a check, yeah
+Just check the foots, yeah
+Pass this to my daughter, I'ma show her what it took (yeah)
+Baby mama cover Forbes, got these other bitches shook, yeah
+Ye-ah""",
+ "Goosebumps" : """
+Yeah
+7:30 in the night, yeah
+Ooh-ooh, ooh
+I get those goosebumps every time, yeah, you come around, yeah
+You ease my mind, you make everything feel fine
+Worried 'bout those comments, I'm way too numb, yeah
+It's way too dumb, yeah
+I get those goosebumps every time, I need the Heimlich
+Throw that to the side, yeah
+I get those goosebumps every time, yeah, when you're not around (straight up)
+When you throw that to the side, yeah (it's lit)
+I get those goosebumps every time, yeah
+7-1-3 through the 2-8-1, yeah I'm ridin'
+Why they on me?
+Why they on me? I'm flyin', sippin' low-key
+I be sippin' low-key in Onyx, rider, rider
+When I'm pullin' up right beside ya
+Pop star, lil' Mariah
+When I text, kick game, knowledge
+Throw a stack on the Bible
+Never Snapchat or took molly
+She fall through plenty, her and all her ginnies, yeah
+We at the top floor, right there off Doheny, yeah
+Oh no, I can't fuck with y'all, yeah
+When I'm with my squad I cannot do no wrong, yeah
+Saucin' in the city, don't get misinformed, yeah
+They gon' pull up on you (brrt, brrt, brrt)
+Yeah, we gon' do some things, some things you can't relate
+Yeah, 'cause we from a place, a place you cannot stay
+Oh, you can't go, oh, I don't know
+Oh, back the fuck up off me (brrt, brrt, brrt)
+I get those goosebumps every time, yeah, you come around, yeah
+You ease my mind, you make everything feel fine
+Worried 'bout those comments, I'm way too numb, yeah
+It's way too dumb, yeah
+I get those goosebumps every time, I need the Heimlich
+Throw that to the side, yeah
+I get those goosebumps every time, yeah, when you're not around
+When you throw that to the side, yeah
+I get those goosebumps every time
+Uh, I wanna press my like, yeah, I wanna press my
+I want a green light, I wanna be like
+I wanna press my line, yeah
+I wanna to take that ride, yeah
+I'm gonna press my line
+I want a green light, I wanna be like, I wanna press my
+Mama, dear, spare your feelings
+I'm relivin' moments, peelin' more residual
+I can buy the building, burn the building, take your bitch
+Rebuild the building just to fuck some more
+I can justify my love for you
+And touch the sky for God to stop debatin' war
+Put the pussy on a pedestal (ayy)
+Put the pussy on a high horse
+That pussy to die for
+That pussy to die for
+Peter Piper, picked a pepper
+So I could pick your brain and put your heart together
+We depart the shady parts and party hard, the diamonds yours
+The coupe forever
+My best shot, just might shoot forever like (brrt)
+I get those goosebumps every time, yeah, you come around, yeah
+You ease my mind, you make everything feel fine
+Worried 'bout those comments, I'm way too numb, yeah
+It's way too dumb, yeah
+I get those goosebumps every time, I need the Heimlich
+Throw that to the side, yeah
+I get those goosebumps every time, yeah, when you're not around
+When you throw that to the side, yeah
+I get those goosebumps every time
+ """,
+ "THE SCOTTS" : """
+Let's go
+We see the hype outside (yeah)
+Right from the house, uh
+Took it straight from outside (yeah)
+Straight to the couch
+We put the mic outside (yeah)
+Air that shit out, uh
+You lettin' THE SCOTTS outside (yeah)
+We runnin' the scouts
+Ain't no controllin' the gang (yeah, yeah, yeah)
+They never leave
+I got tats over my veins (yeah)
+'Cause that what I bleed
+She drink a lot of the bourbon (yeah)
+Like she from the street, uh
+We got control of the flows and, huh, uh, uh (yeah, mm)
+We heard that your wave went dry (yeah)
+We floodin' the drought, uh (mm)
+Heard that your hood outside (yeah)
+We added some routes (mm)
+We havin' the goods outside (mm)
+Move it in and out (mm)
+You lettin' THE SCOTTS outside (mm)
+We runnin' the scouts
+Nigga, the cops outside (yeah)
+Lock up the house (yeah, yeah)
+We keep the team on high (huh)
+Some gold in they mouth (yeah, yeah)
+Nigga, the Porsche outside (huh)
+Without the top (yeah, nigga, yeah, yeah)
+She want a mimosa-sa (yeah)
+Bring in the shots (yeah, yeah, yeah)
+Tell these phony bitches, "Beat it"
+With that Photoshoppin' body, Adobe, help me
+She in there makin' panini, she know I got all the bread
+She know me, got it
+On my hustle, havin' visions
+It's been a minute since my niggas done owned it, howdy (huh, huh)
+Cleveland boy, he make 'em pay
+Yes, that Cleveland boy, he done made a way, hey
+Headed for somewhere to go, anywhere cinema these, these
+Niggas don't know where to go
+Gotta keep givin' 'em heat, heat (yeah)
+Time to go double though, time they add up the math, mad
+And I've been dealin' with so many things, havin' so many dreams
+Let's go
+ """
+}
+
+drake = {
+    "In my feelings" : """
+Trap, TrapMoneyBenny
+This sh- got me in my feelings
+Gotta be real with it, yup
+Kiki, do you love me? Are you riding?
+Say you'll never ever leave from beside me
+'Cause I want ya, and I need ya
+And I'm down for you always
+KB, do you love me? Are you riding?
+Say you'll never ever leave from beside me
+'Cause I want ya, and I need ya
+And I'm down for you always
+Look, the new me is really still the real me
+I swear you gotta feel me before they try and kill me
+They gotta make some choices, they runnin' out of options
+'Cause I've been going off, and they don't know when it's stopping
+And when you get to topping, I see that you've been learning
+And when you get to shopping, you spend it like you earned it
+And when you popped off on your ex, he deserved it
+I thought you were the one from the jump that confirmed it
+TrapMoneyBenny, ayy
+I buy you Champagne but you love some Henny
+From the block, like you Jenny
+I know you special, girl, 'cause I know too many
+'Resha, do you love me? Are you riding?
+Say you'll never ever leave from beside me
+'Cause I want ya, and I need ya
+And I'm down for you always
+JT, do you love me? Are you riding?
+Say you'll never ever leave from beside me
+'Cause I want ya, and I need ya
+And I'm down for you always
+Two bad b- and we kissin' in the Wraith
+Kissin'-kissin' in the Wraith, kiss-kissin' in the Wraith
+I need that black card and the code to the safe
+Code to the safe, code-code to the safe-safe
+I show him how that neck work
+F- that Netflix and chill, what's your net-net-net worth?
+'Cause I want ya, and I need ya
+And I'm down for you always (yeah, yeah, yeah, yeah, he bad)
+And I'm down for you always (yeah, yeah, yeah, guess who's back)
+And I'm down for you always
+D-down for you al- (black biggy biggy black biggy black blake)
+D-d-down for you always
+(I got a new boy, and that n- trade!)
+Kiki, do you love me? Are you riding?
+Say you'll never ever leave from beside me
+'Cause I want you, and I need ya
+And I'm down for you always
+KB, do you love me? Are you riding?
+Say you'll never ever leave from beside me
+'Cause I want ya, and I
+Skate and smoke and ride
+Now let me see you
+Bring that a-, bring that a-, bring that a- back
+Bring that a-, bring that a-, bring that a- back
+Now let me see you
+Shawty say the nigga that she with can't hit
+But shawty, I'ma hit it, hit it like I can't miss
+Now let me see you
+Walk that a-, you're the only one I love
+(Walk that a-, walk-walk that a-)
+Now let me see you (Bring that a- back)
+Now let me see you (w-w-walk that a-, you're the only one I love)
+Now let me see you, now let me see you
+Trap, TrapMoneyBenny
+This sh- got me in my feelings
+Gotta be real with it, yup (Now let me see you)
+BlaqNmilD, you a genius, you diggin' me?
+What are y'all talking about?
+You know what? I don't even care, I need a photo with Drake
+Because my Instagram's weak as f-
+I'm just being real, my sh- look    
+""",
+"One Dance" : """
+Baby, I like your style
+Grips on your waist
+Front way, back way
+You know that I don't play
+Streets not safe
+But I never run away
+Even when I'm away
+OT, OT, there's never much love when we go OT
+I pray to make it back in one piece
+I pray, I pray
+That's why I need a one dance
+Got a Hennessy in my hand
+One more time 'fore I go
+Higher powers taking a hold on me
+I need a one dance
+Got a Hennessy in my hand
+One more time 'fore I go
+Higher powers taking a hold on me
+Baby, I like your style
+Strength and guidance
+All that I'm wishin' for my friends
+Nobody makes it from my ends
+I had to bust up the silence
+You know you gotta stick by me
+Soon as you see the text, reply me
+I don't wanna spend time fighting
+We've got no time
+And that's why I need a one dance
+Got a Hennessy in my hand
+One more time 'fore I go
+Higher powers taking a hold on me
+I need a one dance
+Got a Hennessy in my hand
+One more time 'fore I go
+Higher powers taking a hold on me
+Got a pretty girl and she love me long time
+Wine it, wine it, and she love me long time
+Ooh yeah, just steady and wine up
+Back up, back up, back up and wine up
+Back up, back up and wine it
+Girl, just back up, back up, back up and wine down
+Ooh yeah, just steady and wine up
+Back, up, back up and wine it, girl
+Ooh, tell me
+I need to know, where do you wanna go?
+'Cause if you're down, I'll take it slow
+Make you lose control
+Where, where, where
+Where, where, where, where (ooh yeah, very long time)
+(Back, up, back up and wine it, girl)
+'Cause if you're down (back up, back up and)
+'Cause if you're down (back up, back up and)
+'Cause if you're down (back up, back up and)
+I need a one dance
+Got a Hennessy in my hand
+One more time 'fore I go
+Higher powers taking a hold on me
+I need a one dance
+Got a Hennessy in my hand
+One more time 'fore I go
+Higher powers taking a hold on me
+""",
+"Passionfruit" : """
+Hold on, hold on, fuck that
+Fuck that shit
+Hold on, I got to start this motherfuckin' record over again
+Wait a minute
+Fuck that shit
+Still on this motherfuckin' record
+I'ma play this motherfucka for y'all
+Ayy, y'all get some more drinks goin' on
+I'll sound a whole lot better
+Listen
+Seeing you got ritualistic
+Cleansin' my soul of addiction for now
+'Cause I'm fallin' apart
+Yeah, tension
+Between us just like picket fences
+You got issues that I won't mention for now
+'Cause we're fallin' apart
+Passionate from miles away
+Passive with the things you say
+Passin' up on my old ways
+I can't blame you, no, no
+Passionate from miles away
+Passive with the things you say
+Passin' up on my old ways
+I can't blame you, no, no
+Listen
+Harder buildin' trust from a distance
+I think we should rule out commitment for now
+'Cause we're fallin' apart
+Leavin'
+You're just doing that to get even
+Don't pick up the pieces, just leave it for now
+They keep fallin' apart
+Passionate from miles away
+Passive with the things you say
+Passin' up on my old ways
+I can't blame you, no, no
+Passionate from miles away
+Passive with the things you say
+Passin' up on my old ways
+I can't blame you, no, no
+Um, trying to think of the right thing to say
+"""
+}
+
+nata = {
+    "Diamantes" : """
+Traigo la muñeca bien repleta de diamantes
+Es cierto que ya no soy el mismo que yo era antes
+Tengo mujeres esperándome en todas partes
+Entre menos contesto, más me dejan mensajes
+El cuerpo tumbado lo siento, es la codeína
+Las nalgas de una morrita me brincan encima
+Yo le compro Balenciaga, ella no quiere Adidas
+Quiere una baby nueva para fumar sativa y
+Prendó un cigarrito nomás pa tirar al viento
+Lo malo se pasa cuando fumo y lo siento
+La noche no se acaba y el sol ya saliendo
+Soda de piña en los lados se va escurriendo
+El doble vaso en mi mano siempre está lleno
+Un waxesito nomás pa pasar el tiempo, hey
+Nos vemos luego
+Y puros corridos tumbados, viejones
+¡Eah!
+Hablamos luego, baby, voy aterrizando
+Acá por Miami el solecito está pegando
+A mí las envidias ya me tienen sin cuidado
+No me quita el sueño, yo solo sigo fumando
+Mi madre no sufre y ahora luce diferente
+No se preocupa, porque ya no hay ningún pendiente
+Traigo la muñeca bien repleta de diamantes
+Es cierto que ya no soy el mismo que yo era antes
+Prendo un cigarrito nomás pa tirar al viento
+Lo malo se pasa cuando fumo y lo siento
+La noche no se acaba y el sol ya saliendo
+Soda de piña en los lados se va escurriendo
+El doble vaso en mi mano siempre está lleno
+Un waxesito nomás pa pasar el tiempo
+Nos vemos luego    
+""",
+"Disfruto lo malo" : """
+Como de costumbre
+Navego en la lumbre
+Ganando billetes
+Rifando mi suerte
+Sigo en los negocios
+Tengo buenos socios
+Allá por Tijuana
+Mi equipo se jala
+Ando bien tumbado
+Los ojos cerrados
+La nariz polveando
+Me siento viajado
+Traigo buena clika, me la rolo con los morros
+Yo controlo los negocios, pues mi padre me enseñó
+De La Perla pa Tijuana, unas horas si me llaman
+Cotorreando yo me jalo, chale, no hay fijón
+Puros corridos tumbados, compa Nata
+Así es, compa Junior, ¡ea!
+No solo lo bueno
+Disfruto lo malo
+Los buenos recuerdos
+Los tragos amargos
+Morras tengo varias
+No quiero a ninguna
+Buscan el dinero
+No es por ser culero
+Sirvan de lavada
+Agarrando viada
+Todos bien contentos
+Disfruto el momento
+Traigo buena clika, me la rolo con los morros
+Yo controlo los negocios, pues mi padre me enseñó
+De La Perla pa Tijuana, unas horas si me llaman
+Cotorreando yo me jalo, chale, no hay fijón
+""",
+"Carlitos" : """
+¿Quién iba a pensar?
+Que a Carlitos todo un día se le iba a dar
+La cocina, lo que le fue a generar
+Billetitos que ahora le gusta gastar
+Todo marcha bien
+Desde morro ya sabía lo que iba a hacer
+El negocio lo supo bien atender
+Hoy sin miedo se pasea en el M3
+Fue duro saber
+Que los gringos lo quisieron aprehender
+Lo soltaron, lo peor que fueron a hacer
+Hoy trabaja directo con el cartel
+Del crimen organizado
+Somos gente, traemos buen encargo
+Manejamos un producto
+Varios lo han llamado
+Lo más fino que existe aquí en el mercado
+Pero no todo es bonito, también batallamos
+Se pierde gente, se pierden carros
+Hace un año me robaron
+No se me ha olvidado
+Pero ya duplicamos ese mandado
+¿O no, compa Carlos?
+Y puros corridos tumbados, viejones
+¡Ea!
+Me dicen: "Carlitos"
+Es la clave que manejo en el humito
+Por Sonora me divierto y me desquito
+Con mis primos, celebrando el calorcito
+A los traicioneros
+No los nombro, pero no quiero ni un "pero"
+Cuando los encuentre, van pa'l agujero
+Los que gritan "fiel", esos son los primeros
+Se me antoja una lavada
+Traigan las guitarras, también banda
+Y esta noche se amanece
+Con toda la plebada
+Disfrutando y tirando con armas largas
+Pero no todo es bonito, también batallamos
+Se pierde gente, se pierden carros
+Hace un año me robaron
+No se me ha olvidado
+Pero ya duplicamos ese mandado
+"""
+}
+
+songs = {
+    "Travis Scott" : travis,
+    "Drake" : drake,
+    "Natanael Cano" : nata
+}
+
+
+
+
+def obtener_key(diccionario, autor):
+    if autor in diccionario:
+        return list(diccionario[autor].keys())
+    else:
+        return "Artista No encontrado"
+    
+def obtenerCancion(diccionario,autor,cancion):
+    if autor in diccionario and cancion in diccionario[autor]:
+        return diccionario[autor][cancion]
+    else:
+        return "No existe esa cancion en la biblioteca"
+
+
+
+
+while True:
+
+
+    print(list(songs))
+    autor = input("Ingresa El Nombre De Un Artista: ")
+    indiceCanciones = obtener_key(songs,autor)
+    print("Canciones: ", indiceCanciones)
+
+
+    letraCancion = input("Ingresa Nombre de cancion: ")
+    MostrarCancion = obtenerCancion(songs,autor,letraCancion)
+    print("Esta es la cancion : ", MostrarCancion)
+
+
+    nueva_seleccion = input("Pulsa * para hacer una nueva selección, o cualquier otra tecla para continuar: ")
+    if nueva_seleccion == "*":
+        continue
+
+
+    else:
+        print("Nada")
